f63fdcc4d7b0cf19e08f0a6698c1328c12daf491
yukinotech <yukinotech@gmail.com>
2021-12-13 14:40:11 +0800

    feat(jest): add jest

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # JSBD
-
 ## a JSBI style polyfill
+
+because use es module in node , better use node >= 14
--- test/isLiteral.test.js
+++ test/isLiteral.test.js
@@ -11,6 +11,37 @@ import { isLiteral } from "../dist/utils.js"
 // 1180
 // 01180 ❌
 // -332
+
+test('+0', () => {
+  expect(isLiteral('+0')).toBeTruthy()
+});
+test('-0', () => {
+  expect(isLiteral('-0')).toBeTruthy()
+});
+test('0', () => {
+  expect(isLiteral('0')).toBeTruthy()
+});
+test('0.000', () => {
+  expect(isLiteral('0.000')).toBeTruthy()
+});
+test('0.01000', () => {
+  expect(isLiteral('0.01000')).toBeTruthy()
+});
+test('0111.111', () => {
+  expect(isLiteral('0111.111')).toBeNull()
+});
+test('118.01', () => {
+  expect(isLiteral('118.01')).toBeTruthy()
+});
+test('1180', () => {
+  expect(isLiteral('1180')).toBeTruthy()
+});
+test('01180', () => {
+  expect(isLiteral('01180')).toBeNull()
+});
+test('25456456464.06565', () => {
+  expect(isLiteral('25456456464.06565')).toBeTruthy()
+});
 test('-332', () => {
-  expect(isLiteral('-332')).toBe(3);
+  expect(isLiteral('-332')).toBeTruthy()
 });
